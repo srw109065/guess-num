0ea248067515bbd76ef34dceda59d6435bd122fc
Dio <srw109065@gmail.com>
2020-11-21 19:37:31 +0800

    '更新'

--- r.py
+++ r.py
@@ -1,21 +1,26 @@
 import random
 r = random.randint(1,100)
 
+count = 0
+
 while True:
 	number = input('請猜數字: ')
 	number = int(number)
-
+	count = count + 1
 	if number == r:
 		print('猜對囉!')
+		print("你猜測" , count , "次")
 		break
 	elif number < r:
-		print("數字太小囉", r)
+		print("數字太小囉")
 		if r - number < 5:
 			print("很接近囉")
 	elif number > r:
-		print("數字太大囉!", r)
+
+		print("數字太大囉!")
 		if number - r < 5:
 			print("很接近囉")
+	print("你猜測" , count , "次")
 
 	 
 		
